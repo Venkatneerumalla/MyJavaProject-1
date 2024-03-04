90b7bd2f247efd6f171ef6ab97b5013550339a58
Vaishnavichary49 <vaishnavichary49@gmail.com>
2024-03-04 16:38:52 +0530

    first commit

---
+++ src/main/java/com/anudip/hibernateproject/App.java
@@ -0,0 +1,13 @@
+package com.anudip.hibernateproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
